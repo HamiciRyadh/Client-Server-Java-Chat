97d51df438fdf537ac0fa51eef93b03671db1ff0
hamiciryadh <hamiciryadh@gmail.com>
2021-01-04 19:11:07 +0100

    altering

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         if (args.length < 2) {
             System.err.println("Too few arguments, the correct syntax is : <ip address> <port> ...");
-            System.out.println("321");
+            System.out.println("123321");
             System.exit(1);
         }
 
